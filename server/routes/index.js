21a92958dcd5413e2ac199f60215d09f91a2c2d3
Pham Van Dung <dungpv10@gmail.com>
2017-12-05 03:41:23 +0700

    Ignore

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import ssr from "./ssr";
+const express = require("express");
+// import ssr from "./ssr";
 
 const app = express();
 
@@ -7,7 +7,10 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.use("/*", ssr);
+
+app.get("/", (req, res) => {
+    res.send("vaoooo");
+})
 
 app.listen(3000, () => {
     console.log("Hello World listening on port 3000!");
